4ce00bd9ca4ba46c279290764887af6b77a74b2d
arehor <arehor@me.com>
2024-07-27 12:28:27 -0300

    Second commit and push

---
+++ Test2GitHub.R
@@ -0,0 +1,3 @@
+Print ("This is a new project in the new RStudio verssion. withnew 1TSSD")
+Print ("the intention is to uplodetd to GitHub")
+Print ("the intention is to uplodetd to GitHub, frist time wasen't working")
