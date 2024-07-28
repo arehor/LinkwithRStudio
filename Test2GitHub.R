4bf5a996e2943163048d4cc860dbafbf9aa36aa4
arehor <arehor@me.com>
2024-07-28 13:54:38 -0300

    second commit message !

--- Test2GitHub.R
+++ Test2GitHub.R
@@ -1,3 +1,4 @@
 Print ("This is a new project in the new RStudio verssion. withnew 1TSSD")
 Print ("the intention is to uplodetd to GitHub")
 Print ("the intention is to uplodetd to GitHub, frist time wasen't working")
+Print("one more time!")
